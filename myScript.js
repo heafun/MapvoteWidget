4525cdf255888d6a1c6e43e6929f95e836882f5d
heafun <marc.rosenberger100@gmail.com>
2017-08-02 15:30:13 +0200

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,26 +1,5 @@
-var image = document.getElementById("img1");
-
-for(var i=0; i<document.getElementsByClassName("button").length; i++){
-document.getElementsByClassName("button")[i].onclick = function() {
-    var id = this.id.split("")[0];
-    var oid;
-    if(this.id<=10){
-      oid=(this.id)*11
-    } else {
-      oid=id;
-    }
-
-    document.getElementById('img'+id).classList.add("crossfade1");
-    document.getElementById("spacing"+this.id).classList.add("voted");
-    this.innerHTML = "BANNED";
-    this.classList.add("disable");
-    document.getElementById(oid).classList.add("hide");
-    document.getElementById(oid).onclick = function() {}
-  }
-}
-
-function loadpictures() {
-    for(var i = 1; i<=1 ; i++){
-      document.getElementById("img"+i).classList.add("image1");
-    }
+function ban (id){
+    document.getElementById("btnl"+id).style.opacity = 0;
+    document.getElementById("btnr"+id).style.opacity = 0;
+    document.getElementById("pic"+id).style.opacity = 0;
 }
